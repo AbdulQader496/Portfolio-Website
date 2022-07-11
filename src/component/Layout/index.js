237ce113bd3dfa9b99d9fea9c5ab276a15d8c65a
AbdulQader496 <gm.abdul.qader@gmail.com>
2022-07-12 05:11:29 +0800

    issue fixed

---
+++ src/component/Layout/index.js
@@ -0,0 +1,20 @@
+//import { Outlet } from 'react-router-dom'
+// import Sidebar from '../Sidebar/'
+
+import './index.scss'
+
+
+const Layout = () => {
+
+    return <>
+
+        <h1>Hello</h1>
+        
+      
+    
+        </>
+
+ 
+}
+
+export default Layout
