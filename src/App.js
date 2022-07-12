8933f029c6d265d6dd62956c8b6dcb49f5982c4b
AbdulQader496 <gm.abdul.qader@gmail.com>
2022-07-13 07:51:37 +0800

    Email, About, Contact page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import {Routes, Route} from 'react-router-dom';
-import Layout from './component/Layout/index'
-// import Sidebar from './components/Sidebar';
+import Layout from './component/Layout/index.js'
+import Home from './component/Home/index.js';
 import './App.scss';
+import About from './component/About/index.js';
+import Contact from './component/Contact/index.js';
 
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Layout />}>
-      {/* <Route path="/" element={<Sidebar />}></Route> */}
+        <Route index element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/contact" element={<Contact />}/>
       </Route> 
     </Routes>
     </>
